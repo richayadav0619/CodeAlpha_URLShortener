76333ab1234ae1c73737ac9fe668ad44256c47ca
richayadav0619 <richayadav0619@gmail.com>
2026-07-19 21:34:53 +0530

    fix: update server port from 8000 to 8001 for consistency

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ dotenv.config({
 
 connectDB()
     .then(() => {
-        app.listen(process.env.PORT || 8000, () => {
+        app.listen(process.env.PORT || 8001, () => {
             console.log(`⚙️ Server running on port ${process.env.PORT}`);
         });
     })
